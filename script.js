7ebacfdd2b95fce9578ab3326cb4cd306c924885
Hristo Papucharov <hpapucharov@gmail.com>
2021-03-17 12:02:14 -0500

    Starting Game Logic

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ board = document.querySelector('.board')
 divs = document.querySelectorAll('.board div');
 div = document.querySelector('.board div');
 displayCurrentPlayer = document.querySelector('#current_player');
+whoWon = document.querySelector('#whoWon');
 let currentPlayer = "player1";
+let whoWonInitial = "No winner Yet";
+
 
 //Adding event listeners for the board divs
 divs.forEach(element => {
   element.addEventListener('click', divsArray);
+
 })
 
 //Creating an array with indexes from the divs[i]
@@ -29,7 +33,17 @@ function divsArray(e) {
   else {
     console.log("Player error");
   }
+
+  whoWon.innerHTML = whoWonInitial;
+  if (divs[0].classList.contains("player1") && divs[1].classList.contains("player1") && divs[2].classList.contains("player1")){
+    whoWon.innerHTML = "Player1 Won"
+
+
+  }
 }
+
+
+
 
 //        <div class="" id="0-0">0-0</div>
 //         <div class="" id="0-1">0-1</div>
@@ -40,8 +54,4 @@ function divsArray(e) {
 //         <div class="" id="2-0">2-0</div>
 //         <div class="" id="2-1">2-1</div>
 //         <div class="" id="2-2">2-2</div>
-//Game Logic
 
-// function gameLogic() {
-//
-// }
