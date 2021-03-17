cf60617b2df435cde620a20edbe55f0bc6f4a688
Hristo Papucharov <hpapucharov@gmail.com>
2021-03-16 20:36:44 -0500

    4 Bronze Achieved

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Script loaded")
+
+board = document.querySelector('.board')
+divs = document.querySelectorAll('.board div');
+div = document.querySelector('.board div');
+displayCurrentPlayer = document.querySelector('#current_player');
+let currentPlayer = "player1";
+
+//Adding event listeners for the board divs
+divs.forEach(element => {
+  element.addEventListener('click', divsArray);
+})
+//Creating an array with indexes from the divs[i]
+function divsArray(e) {
+  const divsArray = Array.from(divs);
+  const index = divsArray.indexOf(e.target);
+  displayCurrentPlayer.innerHTML = currentPlayer;
+//This adds the css style to the click event plus updating current player
+  if (currentPlayer === "player1") {
+    divs[index].classList.add("player1")
+    currentPlayer = "player2"
+  }
+  else if (currentPlayer === "player2") {
+    divs[index].classList.add("player2")
+    currentPlayer = "player1"
+  }
+  else {
+    console.log("Player error");
+  }
+
+}
