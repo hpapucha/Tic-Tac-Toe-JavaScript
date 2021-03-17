3877caa26be2502360c48531c0e29a0b0711d59d
Hristo Papucharov <hpapucharov@gmail.com>
2021-03-17 14:37:19 -0500

    Logic Update

--- script.js
+++ script.js
@@ -4,43 +4,70 @@ divs = document.querySelectorAll('.board div');
 div = document.querySelector('.board div');
 displayCurrentPlayer = document.querySelector('#current_player');
 whoWon = document.querySelector('#whoWon');
-let currentPlayer = "player1";
+let currentPlayer = "Red Player";
 let whoWonInitial = "No winner Yet";
 
 
 //Adding event listeners for the board divs
 divs.forEach(element => {
-  element.addEventListener('click', divsArray);
-
+  element.addEventListener('click', divsArray, { once: true } );
+  if (whoWon.innerHTML === "Player1 Won"){
+    element.removeEventListener("click", divsArray, true);
+  }
 })
 
-//Creating an array with indexes from the divs[i]
-
 function divsArray(e) {
+
   const divsArray = Array.from(divs);
   const index = divsArray.indexOf(e.target);
 
   displayCurrentPlayer.innerHTML = currentPlayer;
 //This adds the css style to the click event plus updating current player
-  if (currentPlayer === "player1") {
+  if (currentPlayer === "Red Player") {
     divs[index].classList.add("player1")
-    currentPlayer = "player2"
+    currentPlayer = "Blue Player"
+
   }
-  else if (currentPlayer === "player2") {
+  else if (currentPlayer === "Blue Player") {
     divs[index].classList.add("player2")
-    currentPlayer = "player1"
+    currentPlayer = "Red Player"
   }
   else {
     console.log("Player error");
   }
 
+
+
   whoWon.innerHTML = whoWonInitial;
-  if (divs[0].classList.contains("player1") && divs[1].classList.contains("player1") && divs[2].classList.contains("player1")){
-    whoWon.innerHTML = "Player1 Won"
-
-
+  //Index 0 Row
+  if (divs[0].classList.contains("player1") && divs[1].classList.contains("player1") && divs[2].classList.contains("player1")) {
+    whoWon.innerHTML = "Player Red Won"
+    //  divs.setAttribute("style","pointer-events: none")
   }
+  else if(divs[0].classList.contains("player2") && divs[1].classList.contains("player2") && divs[2].classList.contains("player2")){
+    whoWon.innerHTML = "Player Blue Won"
+  }
+  //Index 0-8 Vertical
+  else if(divs[0].classList.contains("player1") && divs[4].classList.contains("player1") && divs[8].classList.contains("player1")){
+  whoWon.innerHTML = "Player Red Won"
+  }
+  else if(divs[0].classList.contains("player2") && divs[4].classList.contains("player2") && divs[8].classList.contains("player2")){
+    whoWon.innerHTML = "Player Blue Won"
+  }
+  //Index 0-6 Column
+  else if(divs[0].classList.contains("player1") && divs[3].classList.contains("player1") && divs[6].classList.contains("player1")){
+    whoWon.innerHTML = "Player Red Won"
+  }
+  else if(divs[0].classList.contains("player2") && divs[3].classList.contains("player2") && divs[6].classList.contains("player2")){
+    whoWon.innerHTML = "Player Blue Won"
+  }
+
+
+
+//end
 }
+
+
 
 
 
