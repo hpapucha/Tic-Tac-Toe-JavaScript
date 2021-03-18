60bf9a47eb9ff38c302f5f4beb2ec70327f9be7c
Hristo Papucharov <hpapucharov@gmail.com>
2021-03-18 12:04:31 -0500

    Updated CSS v2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ redCounter = document.querySelector('#redCounter');
 blueCounter = document.querySelector('#blueCounter');
 refreshButton = document.querySelector('#btn');
 let currentPlayer = "Red Player";
-let whoWonInitial = "No winner yet!";
+let whoWonInitial = "No winner!";
 let redCount = 0;
 let blueCount = 0;
 let endGame = false;
@@ -56,70 +56,70 @@ function divsArray(e) {
   whoWon.innerHTML = whoWonInitial;
   //Index 0 Row
   if (divs[0].classList.contains("player1") && divs[1].classList.contains("player1") && divs[2].classList.contains("player1")) {
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
 
   }
   else if(divs[0].classList.contains("player2") && divs[1].classList.contains("player2") && divs[2].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 0-8 Vertical
   else if(divs[0].classList.contains("player1") && divs[4].classList.contains("player1") && divs[8].classList.contains("player1")){
-  whoWon.innerHTML = "Player Red Won"
+  whoWon.innerHTML = "Red Won!"
   }
   else if(divs[0].classList.contains("player2") && divs[4].classList.contains("player2") && divs[8].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 0 Column
   else if(divs[0].classList.contains("player1") && divs[3].classList.contains("player1") && divs[6].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
   }
   else if(divs[0].classList.contains("player2") && divs[3].classList.contains("player2") && divs[6].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 1 Column
   else if(divs[1].classList.contains("player1") && divs[4].classList.contains("player1") && divs[7].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
   }
   else if(divs[1].classList.contains("player2") && divs[4].classList.contains("player2") && divs[7].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 2-6 Vertical
   else if(divs[2].classList.contains("player1") && divs[4].classList.contains("player1") && divs[6].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
   }
   else if(divs[2].classList.contains("player2") && divs[4].classList.contains("player2") && divs[6].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 2 Column
   else if(divs[2].classList.contains("player1") && divs[5].classList.contains("player1") && divs[8].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
   }
   else if(divs[2].classList.contains("player2") && divs[5].classList.contains("player2") && divs[8].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 3 Row
   else if(divs[3].classList.contains("player1") && divs[4].classList.contains("player1") && divs[5].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
 
   }
   else if(divs[3].classList.contains("player2") && divs[4].classList.contains("player2") && divs[5].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //Index 6 Row
   else if(divs[6].classList.contains("player1") && divs[7].classList.contains("player1") && divs[8].classList.contains("player1")){
-    whoWon.innerHTML = "Player Red Won"
+    whoWon.innerHTML = "Red Won!"
   }
   else if(divs[6].classList.contains("player2") && divs[7].classList.contains("player2") && divs[8].classList.contains("player2")){
-    whoWon.innerHTML = "Player Blue Won"
+    whoWon.innerHTML = "Blue Won!"
   }
   //If a winner is decided increment the red/blue counters
-  if (whoWon.innerText === "Player Red Won"){
+  if (whoWon.innerText === "Red Won!"){
     redCount += 1;
     redCounter.innerText = redCount;
     removeEventListeners()
 
   }
-  else if(whoWon.innerText === "Player Blue Won"){
+  else if(whoWon.innerText === "Blue Won!"){
     blueCount += 1;
     blueCounter.innerText = blueCount;
     removeEventListeners()
